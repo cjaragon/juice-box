b72cc2a3cd5400b44214eb3487bdfccc53a69e3c
Chris Aragon <caragon.tpp@gmail.com>
2021-01-29 18:13:48 -0600

    Controller is finished, plugged in and tested.

--- server/controllers/catalog-controller.js
+++ server/controllers/catalog-controller.js
@@ -1,15 +1,50 @@
 let songs = [
     {
         id: 0,
-        song: "Christmas in L.A.",
-        Artist: "Vulfpeck",
+        title: "Christmas in L.A.",
+        artist: "Vulfpeck",
+        genre: "Funk"
+    },
+    {
+        id: 1,
+        title: "Back Pocket",
+        artist: "Vulfpeck",
         genre: "Funk"
     }
 ]
-let id = 1
+let id = 2
 
 module.exports = {
     sendSongs: (req, res) => {
+        res.status(200).send(songs)
+    },
+    addSong: (req, res) => {
+        const {title, artist, genre} = req.body
+        songs.push({id, title, artist, genre})
+        id++
+        res.status(200).send(songs)
+    },
+    editGenre: (req, res) => {
+        const {genre} = req.body
+        const updateId = req.params.id
+        const songIndex = songs.findIndex(song => song.id === +updateId)
+        let song = songs[songIndex]
+        console.log(songIndex)
+
+        songs[songIndex] = {
+            id: song.id,
+            title: song.title,
+            artist: song.artist,
+            genre: genre || song.genre
+        }
+
+        res.status(200).send(songs)
+    },
+    deleteSong: (req, res) => {
+        const deleteId = req.params.id
+        const songIndex = songs.findIndex(song => song.id === +deleteId)
+        songs.splice(songIndex, 1)
+
         res.status(200).send(songs)
     }
 }
--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const catCtrl = require('./controllers/catalog-controller.js')
 app.use(express.json())
 
 app.get('/api/songs', catCtrl.sendSongs)
+app.post('/api/songs', catCtrl.addSong)
+app.put('/api/songs/:id', catCtrl.editGenre)
+app.delete('/api/songs/:id', catCtrl.deleteSong)
 
 const port = 4040
 app.listen(port, () => {
